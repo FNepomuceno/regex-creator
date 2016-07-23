a9086125b1fdbce938fbfcb295a7635f89f9f3f5
Francis Nepomuceno <francisnepo@gmail.com>
2016-07-22 20:45:07 -0400

    Update 0.0.1.2 Added more structural elements

--- regparse.c
+++ regparse.c
@@ -2,23 +2,57 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-typedef int (*CondType)(char, char);
+typedef int (*CondType)(char, char, char);
+typedef struct RegexNode RegexNode;
+typedef struct StateNode StateNode;
+typedef struct CondNode  CondNode;
+typedef struct PathNode  PathNode;
 
-typedef struct a {
-} StateNode;
+struct RegexNode {
+	char **name;
+	RegexNode *nextregex;
+};
 
-typedef struct b {
+struct StateNode {
+	RegexNode *matchedRegexes;
+	PathNode *paths;
+};
+
+//TODO: put this in condition.c
+struct CondNode {
 	CondType type;
 	int ifnegate;
 	char arg1, arg2;
-	struct b *nextcond;
-} CondNode;
+	CondNode *nextcond;
+};
 
-typedef struct c {
-	StateNode *src, *dst;
+//TODO: put this in path.c
+struct PathNode {
+	StateNode *dst;
 	CondNode *conds;
-} PathNode;
+	PathNode *nextPath;
+};
 
+//TODO: put this function in condition.c
+int meetsCond(char input, CondNode *cond) {
+	CondType condFunc = cond->type;
+	char inp1 = cond->arg1, inp2 = cond->arg2;
+	return condFunc(input, inp1, inp2) != cond->ifnegate;
+}
+
+//TODO: put this function in path.c
+int satisfiesPath(char input, PathNode *path) {
+	CondNode *cond = path->conds;
+	while(cond != NULL) {
+		if(!meetsCond(input, cond)) {
+			return 0; // false
+		}
+		cond = cond->nextcond;
+	}
+	return 1; // true
+}
+
+//TODO: put these 5 functions in condition.c
 int matches(char input, char c, char ignore) {
 	return input == c;
 }
