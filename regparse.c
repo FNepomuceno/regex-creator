6d9302c0d01f26c5e047aef90f47387d20131f97
Francis Nepomuceno <francisnepo@gmail.com>
2016-07-23 22:09:25 -0400

    Update 0.0.1.3 Added some more structural elements

--- regparse.c
+++ regparse.c
@@ -10,12 +10,15 @@ typedef struct PathNode  PathNode;
 
 struct RegexNode {
 	char **name;
+	char **label;
 	RegexNode *nextregex;
 };
 
+//TODO: put this in state.c
 struct StateNode {
 	RegexNode *matchedRegexes;
 	PathNode *paths;
+	int id;
 };
 
 //TODO: put this in condition.c
@@ -30,26 +33,46 @@ struct CondNode {
 struct PathNode {
 	StateNode *dst;
 	CondNode *conds;
-	PathNode *nextPath;
+	PathNode *nextpath;
 };
 
-//TODO: put this function in condition.c
+//TODO: put these 2 functions in condition.c
 int meetsCond(char input, CondNode *cond) {
 	CondType condFunc = cond->type;
 	char inp1 = cond->arg1, inp2 = cond->arg2;
 	return condFunc(input, inp1, inp2) != cond->ifnegate;
 }
 
-//TODO: put this function in path.c
+CondNode *nextCond(CondNode *cond) {
+	if(cond == NULL) return NULL;
+	return cond->nextcond;
+}
+
+//TODO: put these 2 functions in path.c
 int satisfiesPath(char input, PathNode *path) {
 	CondNode *cond = path->conds;
 	while(cond != NULL) {
 		if(!meetsCond(input, cond)) {
 			return 0; // false
 		}
-		cond = cond->nextcond;
+		cond = nextCond(cond);
 	}
 	return 1; // true
+}
+
+PathNode *nextPath(PathNode *path) {
+	if(path == NULL) return NULL;
+	return path->nextpath;
+}
+
+//TODO: put this function in state.c
+PathNode *nextAppropriatePath(char input, PathNode *start) {
+	PathNode *path = start;
+	while(path != NULL) {
+		if(satisfiesPath(input, path)) return path;
+		path = nextPath(path);
+	}
+	return NULL;
 }
 
 //TODO: put these 5 functions in condition.c
